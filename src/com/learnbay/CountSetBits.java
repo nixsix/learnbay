04ff17e388aeb120789a0bcacaae611ef8646ec4
Nikhil <nvora@operative.com>
2020-01-16 05:57:22 +0530

    No of bits needed for a int

---
+++ src/com/learnbay/CountSetBits.java
@@ -0,0 +1,32 @@
+package com.learnbay;
+
+public class CountSetBits {
+
+	public static void main(String[] args) {
+		
+		System.out.println(countSetBitsByRightShift(33));
+		//System.out.println(9&10);
+	}
+
+	private static int countSetBits(int n) {
+		int count = 0;
+		while(n>0) {
+			n = n&(n-1);
+			System.out.println(n + "  ");
+			count++;
+		}
+		return count;
+	}
+	
+	static int countSetBitsByRightShift(int n) 
+    { 
+        int count = 0; 
+        while (n > 0) { 
+            count = count + n & 1; 
+            n >>= 1; 
+        } 
+        return count; 
+    } 
+  
+
+}
