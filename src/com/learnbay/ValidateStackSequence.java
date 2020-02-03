2dd7d4e120b7f794f7d8da4e3fa8347743768ebe
Nikhil <nvora@operative.com>
2020-02-03 21:38:16 +0530

    Own Code

--- src/com/learnbay/ValidateStackSequence.java
+++ src/com/learnbay/ValidateStackSequence.java
@@ -1,45 +1,45 @@
 package com.learnbay;
 
+import static java.lang.System.out;
+
 import java.util.Stack;
 public class ValidateStackSequence {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
-
-		int len = pushed.length;
-		Stack<Integer> stack = new Stack<>();
-		int i = 0;
-		int j = 0;
-		while (i < len) {
-			if (pushed[i] == popped[j]) {
-				j++; //Match is found so this element would be pushed and then poped
-
-				// check for other elements in the stack as well.
-				while (!stack.isEmpty() && stack.peek() == popped[j]) {
-					stack.pop();
-					j++;
-				}
-			}
-			else  //If No Match  found for i, and j index elements , push element
-				stack.add(pushed[i]);
-			
-			i++;
-		}
-
-		while (!stack.isEmpty()) {
-			if (stack.pop() == popped[j])
-				j++;
-			else
-				return false;
-		}
-
-		return stack.isEmpty();
-
-	}
 
 	public static void main(String[] args) {
-		ValidateStackSequence obj = new ValidateStackSequence();
-		int[] pushed = {1,2,3,4,5};
-		int[] poped = {4,5,3,2,1};
-		boolean isValid = obj.validateStackSequences(pushed, poped);
-		System.out.println("Sequence is " + (isValid ? "Valid " : "Not Valid"));
+		int [] pushed = {1,2,3,4,5};
+		int [] popped = {4,5,3,2,1};
+		System.out.println(validateStackSequences(pushed, popped));
+	}
+
+
+	public static boolean validateStackSequences(int[] pushed, int[] popped) {
+		if((pushed == null || popped == null)|| (pushed.length != popped.length)){
+			return false;
+		}   
+		int popPointer = 0;
+		Stack<Integer> s = new Stack<>();
+		for(int i =0;i<pushed.length;i++){
+			s.push(pushed[i]);
+ 			popPointer = checkPop(popped, popPointer, s);
+		}
+		
+		if(!s.isEmpty()|| popPointer!= popped.length-1) {
+			while(!s.isEmpty() && s.peek()==popped[popPointer]) {
+				s.pop();
+			}
+		}
+		if(s.isEmpty()){
+			return true;
+		}
+		return false;
+	}
+
+
+	private static int checkPop(int[] popped, int popPointer, Stack<Integer> s) {
+		while(!s.isEmpty() && popped[popPointer]==s.peek()){
+			s.pop();
+			popPointer++;
+		}
+ 		return popPointer;
 	}
 }
